e4e40975411bf73482ee108f048bbcfc48a9f6b5
jeyreichel <jeyreichel@gmail.com>
2024-08-22 07:45:44 -0400

    feat: add product route

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/product/get', [DashboardController::class, 'index'])->name('product.get');
+Route::post('/product/creat', [DashboardController::class, 'index'])->name('product.create');
